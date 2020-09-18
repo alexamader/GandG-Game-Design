525d4dbd705443ca5f271c6727029c24d8f08f56
ALEX <alexmader@gmail.com>
2020-09-18 09:48:28 -0700

    Update sketch.js

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -1,8 +1,5 @@
-
-//create an empty array called balls
-
-let balls = [];
-
+ballLst = []
+rectLst = []
 function setup() {
   createCanvas(800, 400);
 
@@ -11,37 +8,101 @@ function setup() {
 function draw(){
 	background(220,123,432);
 
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	    balls[i].drawBall();
-      balls[i].moveBall();
-	  }
+    for (x=0;x<ballLst.length;x++){
+      ballLst[x].draw()
+      ballLst[x].grow()
+    }
+    for (x=0;x<rectLst.length;x++){
+      rectLst[x].draw()
+      rectLst[x].grow()
+    }
+
+
+
 }
 
-function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(random(0,800), random(0,400));
-  balls.push(b);
-  console.log(balls);
-}
 
-//ball class from which to create new balls with similar properties.
+class Rectangle {
+  constructor(x,y,a,b) {
+    this.x = x
+    this.y = y
+    this.a = a
+    this.b = b
+    this.color = [random(255),random(255),random(255)]
+    this.pulse = "true"
+    this.counter = 0
+  }
+  draw(){
+    fill(this.color[0],this.color[1],this.color[2]);
+    rect(this.x,this.y,this.a,this.b);
+  }
+  grow(){
+
+    if (this.counter >= 40){
+      this.pulse = "false"
+      this.counter = 0
+    } else if (this.counter <= -40) {
+      this.pulse = "true"
+      this.counter = 0
+    }
+
+    if (this.pulse == "true"){
+
+      this.a += random(2,4)
+      this.b += random(2,4)
+      this.counter++
+    } else {
+      this.a = this.a - random(2,4);
+      this.b = this.b - random(2,4);
+      this.counter = this.counter - 1
+    }
+  }
+
+}
 class Ball {
+  constructor(x,y,a,b) {
+    this.x = x
+    this.y = y
+    this.a = a
+    this.b = b
+    this.color = [random(255),random(255),random(255)]
+    this.pulse = "true"
+    this.counter = 0
+    }
+  draw(){
+    fill(this.color[0],this.color[1],this.color[2]);
+    ellipse(this.x,this.y,this.a,this.b);
+  }
+  grow(){
 
-	constructor(x,y){ //every ball needs an x value and a y value
-		    this.x = x;
-    		this.y = y;
-	}
+    if (this.counter >= 40){
+      this.pulse = "false"
+      this.counter = 0
+    } else if (this.counter <= -40) {
+      this.pulse = "true"
+      this.counter = 0
+    }
 
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill(random(0,270), random(0,270), random(0,270));
-		    ellipse(this.x,this.y,10,10);
-	}
+    if (this.pulse == "true"){
 
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+random(-2,2);
-		this.y = this.y+random(-0.5,0.5);
-	}
-
+      this.a += random(2,4)
+      this.b += random(2,4)
+      this.counter++
+    } else {
+      this.a = this.a - random(2,4);
+      this.b = this.b - random(2,4);
+      this.counter = this.counter - 1
+    }
+  }
+}
+function keyPressed() {
+  if (keyCode == UP_ARROW){
+    let b = new Ball(random(800),random(400),random(50),random(50));
+    ballLst.push(b);
+  }
+  if (keyCode == DOWN_ARROW){
+    let r = new Rectangle(random(800),random(400),random(50),random(50));
+    rectLst.push(r);
+  }
 
 }
