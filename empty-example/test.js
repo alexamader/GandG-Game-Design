86c0726ed9aab8805150cbd4e070ad71c4e209ed
ALEX <alexmader@gmail.com>
2020-09-24 13:03:41 -0700

    test

---
+++ empty-example/test.js
@@ -0,0 +1,35 @@
+let box;
+
+function setup() {
+  createCanvas(800,400);
+
+  box = new Box(20,20,"square");
+  block = createSprite(50,275,100,100);
+
+}
+
+function draw() {
+  background(255,255,255);
+
+
+  box.draw()
+  box.move()
+}
+
+class Box{
+  constructor(x,y,name){
+    this.x = x;
+    this.y = y;
+    this.name = createSprite(this.x,this.y,50,50);
+
+  }
+  draw(){
+    drawSprites()
+  }
+  move(){
+    if ((this.name.collide(block)) == false){
+      this.name.velocity.y += .2;
+    }
+    this.name.collide(block);
+  }
+}
